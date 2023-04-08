7483c4dd55d6e17af04fb741a81b1ed975ff13e8
Panda Repo <128521829+panda-repo@users.noreply.github.com>
2023-04-08 11:16:35 +0700

    Create __init__.py

---
+++ PandapiesMusicBot/__init__.py
@@ -0,0 +1,38 @@
+from PandapiesMusicBot.core.bot import PandapiesBot
+from PandapiesMusicBot.core.dir import dirr
+from PandapiesMusicBot.core.git import git
+from PandapiesMusicBot.core.userbot import Userbot
+from PandapiesMusicBot.misc import dbb, heroku, sudo
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = PandapiesBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()i
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
