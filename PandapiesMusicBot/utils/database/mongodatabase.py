c287f668d25a8380cde6e5e69b7d7724b1e59598
Panda Repo <128521829+panda-repo@users.noreply.github.com>
2023-04-09 09:30:19 +0700

    Update mongodatabase.py

--- PandapiesMusicBot/utils/database/mongodatabase.py
+++ PandapiesMusicBot/utils/database/mongodatabase.py
@@ -1,6 +1,6 @@
 from typing import Dict, List, Union
 
-from PandaliesMusicBot.core.mongo import mongodb
+from PandapiesMusicBot.core.mongo import mongodb
 
 queriesdb = mongodb.queries
 userdb = mongodb.userstats
