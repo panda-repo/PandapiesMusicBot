8081991c498a8c059978a04c1e965e4c961ff5b8
Panda Repo <128521829+panda-repo@users.noreply.github.com>
2023-04-09 09:31:03 +0700

    Update memorydatabase.py

--- PandapiesMusicBot/utils/database/memorydatabase.py
+++ PandapiesMusicBot/utils/database/memorydatabase.py
@@ -1,6 +1,6 @@
 import config
 from config import PRIVATE_BOT_MODE
-from PandaliesMusicBot.core.mongo import mongodb
+from PandapiesMusicBot.core.mongo import mongodb
 
 channeldb = mongodb.cplaymode
 commanddb = mongodb.commands
